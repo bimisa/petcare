6a1b237755dd154c654c7089eb551a720cd3a725
bimisa <iyareegbedion@gmail.com>
2023-10-26 18:57:20 -0400

    allowed host

--- pet_vax/settings.py
+++ pet_vax/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a^mfnc1kx0fpus541#t_jx#uib%@*dtfrbf1h4ptbhuercw!ut
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
